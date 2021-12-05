efba4e12f3f285f827d2bc02ed9fc60443632780
rui-rui-an <1405613785@qq.com>
2021-12-05 23:17:43 +0800

    完成项目初始

---
+++ src/mock/index.js
@@ -0,0 +1,21 @@
+const Mock = require('mockjs');
+
+// 2、获取 mock.Random 对象
+// const random = Mock.Random;
+// console.log(random); // 简单使用就不操作了，需要操作的可以去看文档
+import asyncdata from './data'
+import tableData from './tableData'
+
+
+// 3、基本用法 Mock.mock(url, type, data) // 参数文档 https://github.com/nuysoft/Mock/wiki
+Mock.mock('/echarts/humanrelation', 'get', {
+  code: 200,
+  message: '成功',
+  data: asyncdata
+});
+
+Mock.mock('/echarts/aboutrelation', 'get', {
+  code: 200,
+  message: '成功',
+  data: tableData
+});
